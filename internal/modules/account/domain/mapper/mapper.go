9cb8e84f2d2bbee5bf4a0a32e9e31c91a664aecd
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-25 12:18:06 -0300

    feat(account): mapper dto to model

---
+++ internal/modules/account/domain/mapper/mapper.go
@@ -0,0 +1,19 @@
+package mapper
+
+import (
+	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain"
+	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain/dto"
+)
+
+func ToModel(d dto.CreateRequestDTO) *domain.Account {
+	return &domain.Account{
+		User: domain.User{
+			Firstname: d.Firstname,
+			Lastname:  d.Lastname,
+			Email:     d.Email,
+			CPF:       d.CPF,
+			Password:  d.Password,
+		},
+		Type: domain.Type(d.Type),
+	}
+}
