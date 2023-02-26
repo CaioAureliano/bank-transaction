638230686b7137f910161c52a045579031658ceb
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-26 05:30:22 -0300

    feat(account): repository - get user by email

--- internal/modules/account/repository/repository.go
+++ internal/modules/account/repository/repository.go
@@ -3,11 +3,13 @@ package repository
 import (
 	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain"
 	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain/mapper"
+	"github.com/CaioAureliano/bank-transaction/pkg/model"
 	"gorm.io/gorm"
 )
 
 type Database interface {
 	Create(interface{}) *gorm.DB
+	First(dest interface{}, conds ...interface{}) *gorm.DB
 }
 
 type Repository struct {
@@ -20,4 +22,13 @@ func New(db Database) Repository {
 
 func (r Repository) Create(u *domain.User) error {
 	return r.db.Create(mapper.ToEntity(u)).Error
+}
+
+func (r Repository) GetByEmail(email string) (*model.User, error) {
+	var user model.User
+	if result := r.db.First(&user, "email = ?", email); result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &user, nil
 }
