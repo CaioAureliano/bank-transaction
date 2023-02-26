2aee5c715e3f28df1bd2e58106ba94748aade6c9
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-26 20:36:14 -0300

    test(transaction): create transaction service

---
+++ internal/modules/transaction/service/service_test.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/CaioAureliano/bank-transaction/internal/modules/transaction/domain"
+	"github.com/CaioAureliano/bank-transaction/internal/modules/transaction/domain/dto"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockRepository struct {
+	fnPubMessage        func(*domain.PubMessage) error
+	fnCreateTransaction func(*domain.Transaction) (uint, error)
+}
+
+func (m mockRepository) PubMessage(message *domain.PubMessage) error {
+	if m.fnPubMessage == nil {
+		return nil
+	}
+	return m.fnPubMessage(message)
+}
+
+func (m mockRepository) CreateTransaction(t *domain.Transaction) (uint, error) {
+	if m.fnCreateTransaction == nil {
+		return 0, nil
+	}
+	return m.fnCreateTransaction(t)
+}
+
+func TestCreateTransaction(t *testing.T) {
+
+	repositoryMock := mockRepository{
+		fnCreateTransaction: func(t *domain.Transaction) (uint, error) {
+			return 111, nil
+		},
+	}
+
+	userIdMock := uint(1)
+	reqMock := &dto.TransactionRequestDTO{
+		Value: 10000,
+		Payee: 2,
+	}
+
+	s := New(repositoryMock)
+	transactionID, err := s.CreateTransaction(reqMock, userIdMock)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, transactionID)
+}
