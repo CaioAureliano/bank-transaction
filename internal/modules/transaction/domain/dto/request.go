968f55a7afa2bbb83bf5dc28dd6cb7e174edabcd
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-26 19:22:34 -0300

    fix(transaction): remove payer from request

--- internal/modules/transaction/domain/dto/request.go
+++ internal/modules/transaction/domain/dto/request.go
@@ -2,6 +2,5 @@ package dto
 
 type TransactionRequestDTO struct {
 	Value float64 `json:"value" validate:"required"`
-	Payer uint    `json:"payer" validate:"required"`
 	Payee uint    `json:"payee" validate:"required"`
 }
