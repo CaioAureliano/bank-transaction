c3e8b8efe31a31ad80f7f74caa3dbcc921b44134
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-25 21:00:44 -0300

    fix: change mapper to new associations

--- internal/modules/account/domain/mapper/mapper.go
+++ internal/modules/account/domain/mapper/mapper.go
@@ -3,17 +3,31 @@ package mapper
 import (
 	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain"
 	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain/dto"
+	"github.com/CaioAureliano/bank-transaction/pkg/model"
 )
 
-func ToModel(d dto.CreateRequestDTO) *domain.Account {
-	return &domain.Account{
-		User: &domain.User{
-			Firstname: d.Firstname,
-			Lastname:  d.Lastname,
-			Email:     d.Email,
-			CPF:       d.CPF,
-			Password:  d.Password,
+func ToModel(d dto.CreateRequestDTO) *domain.User {
+	return &domain.User{
+		Firstname: d.Firstname,
+		Lastname:  d.Lastname,
+		Email:     d.Email,
+		CPF:       d.CPF,
+		Password:  d.Password,
+		Account: &domain.Account{
+			Type: domain.Type(d.Type),
 		},
-		Type: domain.Type(d.Type),
+	}
+}
+
+func ToEntity(m *domain.User) *model.User {
+	return &model.User{
+		Firstname: m.Firstname,
+		Lastname:  m.Lastname,
+		Email:     m.Email,
+		CPF:       m.CPF,
+		Password:  m.Password,
+		Account: &model.Account{
+			Type: model.Type(m.Account.Type),
+		},
 	}
 }
