cff287e0150a163412e3c0e834650ae8f613a385
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-26 05:07:32 -0300

    feat(account): map entity to domain model

--- internal/modules/account/domain/mapper/mapper.go
+++ internal/modules/account/domain/mapper/mapper.go
@@ -6,7 +6,7 @@ import (
 	"github.com/CaioAureliano/bank-transaction/pkg/model"
 )
 
-func ToModel(d dto.CreateRequestDTO) *domain.User {
+func RequestToModel(d dto.CreateRequestDTO) *domain.User {
 	return &domain.User{
 		Firstname: d.Firstname,
 		Lastname:  d.Lastname,
@@ -28,6 +28,23 @@ func ToEntity(m *domain.User) *model.User {
 		Password:  m.Password,
 		Account: &model.Account{
 			Type: model.Type(m.Account.Type),
+		},
+	}
+}
+
+func ToModel(entity *model.User) *domain.User {
+	return &domain.User{
+		ID:        entity.ID,
+		Firstname: entity.Firstname,
+		Lastname:  entity.Lastname,
+		CPF:       entity.CPF,
+		Email:     entity.Email,
+		Password:  entity.Password,
+		CreatedAt: entity.CreatedAt,
+		Account: &domain.Account{
+			ID:      entity.Account.ID,
+			Type:    domain.Type(entity.Account.Type),
+			Balance: entity.Account.Balance,
 		},
 	}
 }
