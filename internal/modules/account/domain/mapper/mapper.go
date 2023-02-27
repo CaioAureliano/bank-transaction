30daf1a9e951208d6e59c156ec2a9c585130545d
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-27 05:30:30 -0300

    refactor(account): use new parser structs

--- internal/modules/account/domain/mapper/mapper.go
+++ internal/modules/account/domain/mapper/mapper.go
@@ -4,6 +4,7 @@ import (
 	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain"
 	"github.com/CaioAureliano/bank-transaction/internal/modules/account/domain/dto"
 	"github.com/CaioAureliano/bank-transaction/pkg/model"
+	"github.com/CaioAureliano/bank-transaction/pkg/utils"
 )
 
 func RequestToModel(d dto.CreateRequestDTO) *domain.User {
@@ -19,32 +20,10 @@ func RequestToModel(d dto.CreateRequestDTO) *domain.User {
 	}
 }
 
-func ToEntity(m *domain.User) *model.User {
-	return &model.User{
-		Firstname: m.Firstname,
-		Lastname:  m.Lastname,
-		Email:     m.Email,
-		CPF:       m.CPF,
-		Password:  m.Password,
-		Account: &model.Account{
-			Type: model.Type(m.Account.Type),
-		},
-	}
+func ToEntity(user *domain.User) *model.User {
+	return utils.ParseTo[model.User](user)
 }
 
 func ToModel(entity *model.User) *domain.User {
-	return &domain.User{
-		ID:        entity.ID,
-		Firstname: entity.Firstname,
-		Lastname:  entity.Lastname,
-		CPF:       entity.CPF,
-		Email:     entity.Email,
-		Password:  entity.Password,
-		CreatedAt: entity.CreatedAt,
-		Account: &domain.Account{
-			ID:      entity.Account.ID,
-			Type:    domain.Type(entity.Account.Type),
-			Balance: entity.Account.Balance,
-		},
-	}
+	return utils.ParseTo[domain.User](entity)
 }
