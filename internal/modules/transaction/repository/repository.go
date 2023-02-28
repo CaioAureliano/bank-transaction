96cce1683555be588494b080e98f7d73cc3c5408
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-28 05:10:28 -0300

    feat(transaction): verify if transaction exists by user id and array of status

--- internal/modules/transaction/repository/repository.go
+++ internal/modules/transaction/repository/repository.go
@@ -1,28 +1,39 @@
 package repository
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/CaioAureliano/bank-transaction/internal/modules/transaction/domain"
 	"github.com/CaioAureliano/bank-transaction/internal/modules/transaction/domain/mapper"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
 type Database interface {
 	Create(interface{}) *gorm.DB
+	First(dest interface{}, conds ...interface{}) *gorm.DB
+	Where(query interface{}, args ...interface{}) *gorm.DB
 }
 
 type Queue interface {
 	SendMessage(string) error
 }
 
+type Cache interface {
+	Set(ctx context.Context, k string, v interface{}, expiration time.Duration) *redis.StatusCmd
+}
+
 type Repository struct {
 	db Database
 	q  Queue
+	c  Cache
 }
 
-func New(db Database, b Queue) Repository {
-	return Repository{db, b}
+func New(db Database, b Queue, c Cache) Repository {
+	return Repository{db, b, c}
 }
 
 func (r Repository) CreateTransaction(t *domain.Transaction) (uint, error) {
@@ -34,11 +45,14 @@ func (r Repository) CreateTransaction(t *domain.Transaction) (uint, error) {
 		return 0, result.Error
 	}
 
+	r.c.Set(context.Background(), fmt.Sprint(t.ID), fmt.Sprint(t.Status), time.Minute*1)
+
 	return entity.ID, nil
 }
 
-func (r Repository) ExistsByUserIDAndStatus(userID uint, status []domain.Status) bool {
-	return false
+func (r Repository) ExistsByUserIDAndStatus(payerID uint, status []domain.Status) bool {
+	err := r.db.Where("payer_id = ? AND status IN ?", payerID, status).First(&domain.Transaction{}).Error
+	return err == nil
 }
 
 func (r Repository) PubMessage(message *domain.PubMessage) error {
