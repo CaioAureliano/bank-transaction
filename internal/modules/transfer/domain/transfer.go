83ca9e59199b95f9fa329894ffd55cf01ca1e6ae
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-28 00:21:31 -0300

    feat(transfer): transference domain model with Tranfer logic(method)

---
+++ internal/modules/transfer/domain/transfer.go
@@ -0,0 +1,32 @@
+package domain
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/CaioAureliano/bank-transaction/pkg/model"
+)
+
+type Transference struct {
+	Payer *model.Account
+	Payee *model.Account
+	Value float64
+}
+
+func NewTransference(payer, payee *model.Account, value float64) *Transference {
+	return &Transference{
+		Payer: payer,
+		Payee: payee,
+		Value: value,
+	}
+}
+
+func (t *Transference) Transfer() error {
+	if t.Payer.Balance < t.Value {
+		return errors.New(fmt.Sprintf("payer user account(with ID: %d) without balance", t.Payer.ID))
+	}
+
+	t.Payer.Balance -= t.Value
+	t.Payee.Balance += t.Value
+	return nil
+}
