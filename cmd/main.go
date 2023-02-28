589e403ec06ba94722f9f4cf2c922e8f397de6e3
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-28 05:01:48 -0300

    chore: add migration to init func

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	application "github.com/CaioAureliano/bank-transaction/internal/config"
+	"github.com/CaioAureliano/bank-transaction/internal/config/migration"
 )
+
+func init() {
+	migration.Migrate()
+}
 
 func main() {
 	application.Start()
