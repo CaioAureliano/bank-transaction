0a67371fc0fe7ad4f5c8aa1252b8ce8de2402d8d
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-28 05:11:16 -0300

    feat(transaction): cache injection

--- internal/modules/transaction/module.go
+++ internal/modules/transaction/module.go
@@ -6,12 +6,13 @@ import (
 	"github.com/CaioAureliano/bank-transaction/internal/modules/transaction/service"
 	"github.com/CaioAureliano/bank-transaction/pkg/queue"
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
-func Setup(group fiber.Router, db *gorm.DB) {
+func Setup(group fiber.Router, db *gorm.DB, cache *redis.Client) {
 
-	r := repository.New(db, queue.New())
+	r := repository.New(db, queue.New(), cache)
 
 	s := service.New(r)
 
