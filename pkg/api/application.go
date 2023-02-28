f2a116dd50dd88075aa34037a78e456c6ae5eece
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-28 05:07:34 -0300

    feat: add recovery and idempotency to setup application

--- pkg/api/application.go
+++ pkg/api/application.go
@@ -3,10 +3,13 @@ package api
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/idempotency"
 )
 
 func Setup() *fiber.App {
 	app := fiber.New()
 	app.Use(cors.New())
+	app.Use(recover())
+	app.Use(idempotency.New())
 	return app
 }
