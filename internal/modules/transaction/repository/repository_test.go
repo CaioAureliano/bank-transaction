3f1c3fdadb86905257b018ab4e4a910c28970fc5
CaioAureliano <caio.aureliano1@gmail.com>
2023-03-01 06:02:42 -0300

    chore(transaction): add cache to unit test setup

--- internal/modules/transaction/repository/repository_test.go
+++ internal/modules/transaction/repository/repository_test.go
@@ -1,32 +1,53 @@
 package repository
 
 import (
+	"context"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/CaioAureliano/bank-transaction/internal/modules/transaction/domain"
 	"github.com/CaioAureliano/bank-transaction/pkg/database"
 	"github.com/CaioAureliano/bank-transaction/pkg/utils/test"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 )
 
+type mockCache struct {
+	fnSet func(ctx context.Context, k string, v interface{}, expiration time.Duration) *redis.StatusCmd
+}
+
+func (m mockCache) Set(ctx context.Context, k string, v interface{}, expiration time.Duration) *redis.StatusCmd {
+	if m.fnSet == nil {
+		return nil
+	}
+	return m.fnSet(ctx, k, v, expiration)
+}
+
 func TestCreateTransaction(t *testing.T) {
 	conn, mock, _ := sqlmock.New()
-	defer conn.Close()
 	db := database.Connection(test.DialectorMock(conn))
-	r := New(db, nil)
+
+	cacheMock := mockCache{
+		fnSet: func(ctx context.Context, k string, v interface{}, expiration time.Duration) *redis.StatusCmd {
+			return nil
+		},
+	}
+
+	r := New(db, nil, cacheMock)
 
 	transactionToPersist := &domain.Transaction{
 		PayerID: 1,
 		PayeeID: 2,
 		Value:   1000,
 		Status:  domain.REQUESTED,
+		Message: "",
 	}
 
 	mock.ExpectBegin()
-	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `transactions` (`payer_id`,`payee_id`,`value`,`status`,`created_at`) VALUES (?,?,?,?,?)")).
-		WithArgs(transactionToPersist.PayerID, transactionToPersist.PayeeID, transactionToPersist.Value, transactionToPersist.Status, test.AnyTime{}).
+	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `transactions`")).
+		WithArgs(transactionToPersist.PayerID, transactionToPersist.PayeeID, transactionToPersist.Value, transactionToPersist.Status, transactionToPersist.Message, test.AnyTime{}, test.AnyTime{}).
 		WillReturnResult(sqlmock.NewResult(100, 1))
 	mock.ExpectCommit()
 
