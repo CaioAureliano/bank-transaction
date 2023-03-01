b1d6b1e3212860d079832cf98424f32433e38c5f
CaioAureliano <caio.aureliano1@gmail.com>
2023-03-01 05:49:50 -0300

    fix: return empty string on tests

--- pkg/configuration/getenv.go
+++ pkg/configuration/getenv.go
@@ -5,6 +5,11 @@ import (
 )
 
 func getenv(key string) string {
+	// Return empty string to all variables to unit tests
+	if v, e := os.LookupEnv("ENV"); !e || v != "" {
+		return ""
+	}
+
 	if value, ok := os.LookupEnv(key); ok && value != "" {
 		return value
 	}
