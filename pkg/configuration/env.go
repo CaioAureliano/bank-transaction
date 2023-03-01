6a29a8498beabefbdeacfb44f3ba0a1ddd9e924c
CaioAureliano <caio.aureliano1@gmail.com>
2023-02-28 22:37:20 -0300

    chore: new env variables in config application helper

--- pkg/configuration/env.go
+++ pkg/configuration/env.go
@@ -1,23 +1,27 @@
 package configuration
 
 type EnvironmentVariable struct {
-	PORT       string
-	JWTSECRET  string
-	DBNAME     string
-	DBUSER     string
-	DBPASSWORD string
-	QUEUEURI   string
-	QUEUENAME  string
-	REDISURI   string
+	PORT                    string
+	JWTSECRET               string
+	DBURI                   string
+	DBNAME                  string
+	DBUSER                  string
+	DBPASSWORD              string
+	QUEUEURI                string
+	QUEUENAME               string
+	REDISURI                string
+	AUTHENTICATORSERVICEURL string
 }
 
 var Env = EnvironmentVariable{
-	PORT:       getenv("PORT"),
-	JWTSECRET:  getenv("JWT_SECRET"),
-	DBNAME:     getenv("DB_NAME"),
-	DBUSER:     getenv("DB_USER"),
-	DBPASSWORD: getenv("DB_PASSWORD"),
-	QUEUEURI:   getenv("RABBITMQ_URI"),
-	QUEUENAME:  getenv("RABBITMQ_QUEUE_NAME"),
-	REDISURI:   getenv("REDIS_URI"),
+	PORT:                    getenv("PORT"),
+	JWTSECRET:               getenv("JWT_SECRET"),
+	DBURI:                   getenv("DB_URI"),
+	DBNAME:                  getenv("DB_NAME"),
+	DBUSER:                  getenv("DB_USER"),
+	DBPASSWORD:              getenv("DB_PASSWORD"),
+	QUEUEURI:                getenv("RABBITMQ_URI"),
+	QUEUENAME:               getenv("RABBITMQ_QUEUE_NAME"),
+	REDISURI:                getenv("REDIS_URI"),
+	AUTHENTICATORSERVICEURL: getenv("AUTHENTICATOR_SERVICE_URL"),
 }
